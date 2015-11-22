5aaafe1a368ca53d6f418bce67b0d2a32d10b2f2
ngryman <ngryman@gmail.com>
2015-11-22 16:07:17 +0100

    refactoring and cleanup

--- overrides/bookmarks/src/main.js
+++ overrides/bookmarks/src/main.js
@@ -10,23 +10,20 @@ Bookmark.recent(100).then(function(bookmarks) {
   const $bookmarkTpl = document.querySelector('#template-bookmark')
 
   bookmarks.forEach(function(bookmark) {
-    const $bookmark = document.importNode($bookmarkTpl.content, true)
-    $bookmark.querySelector('.bookmark').dataset.id = bookmark.id
-    $bookmark.querySelector('.bookmark__link').href = bookmark.url
-    $bookmark.querySelector('.bookmark__favicon').src = 'chrome://favicon/' + bookmark.url
-    $bookmark.querySelector('.bookmark__site').innerText = bookmark.site
-    $bookmark.querySelector('.bookmark__title').innerText = bookmark.title
+    const $bookmarkFrag = document.importNode($bookmarkTpl.content, true)
+    $bookmarkFrag.querySelector('.bookmark').__bookmark = bookmark
+    $bookmarkFrag.querySelector('.bookmark__link').href = bookmark.url
+    $bookmarkFrag.querySelector('.bookmark__favicon').src = 'chrome://favicon/' + bookmark.url
+    $bookmarkFrag.querySelector('.bookmark__site').innerText = bookmark.site
+    $bookmarkFrag.querySelector('.bookmark__title').innerText = bookmark.title
 
     const $tagTpl = document.querySelector('#template-tag')
-    const $tags = $bookmark.querySelector('.bookmark__tags')
+    const $tags = $bookmarkFrag.querySelector('.bookmark__tags')
+    $tags.__$bookmark = $bookmarkFrag.querySelector('.bookmark')
 
-    bookmark.tags.forEach(function(tag) {
-      const $tag = document.importNode($tagTpl.content, true)
-      $tag.querySelector('.tags__tag').innerText = tag
-      $tags.appendChild($tag)
-    })
+    renderTags($tags, bookmark.tags)
 
-    $bookmarks.appendChild($bookmark)
+    $bookmarks.appendChild($bookmarkFrag)
   })
 })
 
@@ -43,68 +40,106 @@ $bookmarks.addEventListener('click', function(e) {
   e.preventDefault()
 })
 
-/**
- * Activates inline edit of tags.
- */
-$bookmarks.addEventListener('click', function(e) {
-  const $tags = e.path.find(function(el) {
-    return (el.classList && el.classList.contains('bookmark__tags'))
-  })
-  if (!$tags) return
-
-  editTags($tags)
-})
+/** Activates inline edit of tags. */
+$bookmarks.addEventListener('click', onTagsClick)
 
 /* -------------------------------------------------------------------------- */
 
-function editTags($tags) {
-  const $bookmark = $tags.parentNode.parentNode
+function renderBookmark($container, bookmark) {
+  const $bookmarkTpl = document.querySelector('#template-bookmark')
 
-  Bookmark.get($bookmark.dataset.id).then(function(bookmark) {
-    $tags.innerText = bookmark.tags.join(', ').trim()
-    $tags.setAttribute('contenteditable', true)
-    $tags.focus()
+  const $bookmarkFrag = document.importNode($bookmarkTpl.content, true)
+  $bookmarkFrag.querySelector('.bookmark').__bookmark = bookmark
+  $bookmarkFrag.querySelector('.bookmark__link').href = bookmark.url
+  $bookmarkFrag.querySelector('.bookmark__favicon').src = 'chrome://favicon/' + bookmark.url
+  $bookmarkFrag.querySelector('.bookmark__site').innerText = bookmark.site
+  $bookmarkFrag.querySelector('.bookmark__title').innerText = bookmark.title
 
-    // makes sure cursor is at the end
-    if ($tags.innerText.length > 0) {
-      var selection = window.getSelection()
-      selection.collapse($tags.firstChild, $tags.innerText.length)
-    }
+  const $tags = $bookmarkFrag.querySelector('.bookmark__tags')
+  $tags.__$bookmark = $bookmarkFrag.querySelector('.bookmark')
 
-    // saves bookmark when leaving the edit zone
-    $tags.addEventListener('blur', function() {
-      bookmark.tags = $tags.innerText.split(/, ?/)
-      bookmark.update().then(function() {
-        $tags.removeAttribute('contenteditable')
-        renderTags($tags, bookmark.tags)
-      })
-    })
+  renderTags($tags, bookmark.tags)
 
-    // return key also leaves the field
-    // tab key focuses next bookmark tags
-    $tags.addEventListener('keydown', function(e) {
-      if (13 === e.which) {
-        e.preventDefault()
-        this.blur()
-      }
-      else if (9 === e.which) {
-        e.preventDefault()
-        const direction = e.shiftKey ? 'previous' : 'next'
-        const $next = $bookmark[direction + 'ElementSibling'].querySelector('.bookmark__tags')
-        editTags($next)
-      }
-    })
-  })
+  $bookmarks.appendChild($bookmarkFrag)
 }
 
-function renderTags($tags, tags) {
-  $tags.innerHTML = ''
+function renderTags($container, tags) {
+  $container.innerHTML = ''
 
   const $tagTpl = document.querySelector('#template-tag')
 
   tags.forEach(function(tag) {
-    const $tag = document.importNode($tagTpl.content, true)
-    $tag.querySelector('.tags__tag').innerText = tag
-    $tags.appendChild($tag)
+    const $tagFrag = document.importNode($tagTpl.content, true)
+    $tagFrag.querySelector('.tags__tag').innerText = tag
+    $container.appendChild($tagFrag)
   })
+}
+
+/* -------------------------------------------------------------------------- */
+
+function onTagsClick(e) {
+  const $tags = e.path.find(function(el) {
+    return (el.classList && el.classList.contains('bookmark__tags'))
+  })
+  if (!$tags) return
+  startEditing($tags)
+}
+
+function onTagsBlur(e) {
+  stopEditing(e.target)
+}
+
+function onTagsKeydown(e) {
+  if (13 === e.which) {
+    e.preventDefault()
+    e.target.blur()
+  }
+  else if (9 === e.which) {
+    e.preventDefault()
+    selectTags(e.target, e.shiftKey ? 'previous' : 'next')
+  }
+  else {
+    e.target.dataset.modified = true
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+function startEditing($tags) {
+  const bookmark = $tags.__$bookmark.__bookmark
+
+  $tags.innerText = bookmark.tags.join(', ').trim()
+  $tags.setAttribute('contenteditable', true)
+  $tags.focus()
+
+  // makes sure cursor is at the end
+  if ($tags.innerText.length > 0) {
+    var selection = window.getSelection()
+    selection.collapse($tags.firstChild, $tags.innerText.length)
+  }
+
+  // saves bookmark when leaving the edit zone
+  $tags.addEventListener('blur', onTagsBlur)
+
+  // return key also leaves the field
+  // tab key focuses next bookmark tags
+  $tags.addEventListener('keydown', onTagsKeydown)
+}
+
+function stopEditing($tags) {
+  const bookmark = $tags.__$bookmark.__bookmark
+
+  bookmark.tags = $tags.innerText.trim().split(/, ?/)
+  bookmark.update().then(function() {
+    $tags.removeAttribute('contenteditable')
+    $tags.removeEventListener('blur', onTagsBlur)
+    $tags.removeEventListener('keydown', onTagsKeydown)
+    renderTags($tags, bookmark.tags)
+  })
+}
+
+function selectTags($tags, direction) {
+  const $bookmark = $tags.__$bookmark
+  const $next = $bookmark[direction + 'ElementSibling'].querySelector('.bookmark__tags')
+  startEditing($next)
 }
