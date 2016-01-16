41c997e8150fe1e4e718bfc3d11cbc92f6fc3c2b
ngryman <ngryman@gmail.com>
2016-01-16 02:16:11 +0100

    uses font version of material icons

--- shared/components/search/search.jsx
+++ shared/components/search/search.jsx
@@ -5,9 +5,7 @@ export default class Search extends Component {
     return (
       <form className="search">
         <input className="search__input" type="search" onChange={::this.handleChange} placeholder="Recherche rapide" />
-        <svg className="search__icon" viewBox="0 0 48 48">
-          <path fill="currentColor" d="M31 28h-1.59l-.55-.55C30.82 25.18 32 22.23 32 19c0-7.18-5.82-13-13-13S6 11.82 6 19s5.82 13 13 13c3.23 0 6.18-1.18 8.45-3.13l.55.55V31l10 9.98L40.98 38 31 28zm-12 0c-4.97 0-9-4.03-9-9s4.03-9 9-9 9 4.03 9 9-4.03 9-9 9z"/>
-        </svg>
+        <i className="search__icon material-icons">search</i>
       </form>
     )
   }
