02ab614bf186d2b18471d75162b572fa41b667ca
ngryman <ngryman@gmail.com>
2016-01-16 02:04:06 +0100

    extracts search component & use of material icons

--- shared/components/header/header.jsx
+++ shared/components/header/header.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react'
+import Search from '../search/search.jsx'
 
 export default class Header extends Component {
   render() {
     return (
       <header className="header">
         <a className="header__title" href="#">Bookmarks Baby!</a>
-        <input className="header__search" type="search" onChange={::this.handleChange} placeholder="Recherche rapide" />
+        <Search onSearch={this.props.onSearch} />
       </header>
     )
-  }
-
-  handleChange(e) {
-    if (this.props.onSearch) {
-      this.props.onSearch(e.target.value)
-    }
   }
 }
